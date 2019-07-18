02e74899377ef35880a044a7f2ba529843f274bf
Trevor Davis <trevor.l.davis@gmail.com>
2019-07-18 12:19:54 -0700

    test: Skip text diagrams on AppVeyor

--- tests/testthat/test_unicode.R
+++ tests/testthat/test_unicode.R
@@ -6,10 +6,9 @@ test_that("dimensions", {
     expect_equal(yrange(df), c(0.5, 4.5))
 })
 test_that("text diagrams", {
-    string <- "\u2600\u20dd\u2503\u263d\u20dd\u2502"
-    expect_output(cat(string), string)
-    # expect_output(cat_piece(df_fide_chess()),
-    #               "\u2600\u20dd\u2503\u263d\u20dd\u2502")
-    # expect_output(cat_piece(df_xiangqi()), 
-    #               "\u265b\u20dd\u2501\u254b\u2501\u2e38\u20dd")
+    skip_on_appveyor() 
+    expect_output(cat_piece(df_fide_chess()),
+                  "\u2600\u20dd\u2503\u263d\u20dd\u2502")
+    expect_output(cat_piece(df_xiangqi()), 
+                  "\u265b\u20dd\u2501\u254b\u2501\u2e38\u20dd")
 })
