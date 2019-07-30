a0c503d85619ed6b4156413aed02e1a429a81e2d
Trevor Davis <trevor.l.davis@gmail.com>
2019-07-29 17:55:52 -0700

    feat: Preliminary Unicode text diagram support

--- R/unicode.R
+++ R/unicode.R
@@ -417,11 +417,13 @@ warning_rotation <- function(char, angle) {
 }
 
 xrange <- function(df) {
+    if(nrow(df) == 0) { return (c(NA_real_, NA_real_)) }
     xleft <- ifelse(grepl("tile", df$piece_side), df$x-1, df$x-0.5)
     xright <- ifelse(grepl("tile", df$piece_side), df$x+1, df$x+0.5)
     c(min(xleft), max(xright))
 }
 yrange <- function(df) {
+    if(nrow(df) == 0) { return (c(NA_real_, NA_real_)) }
     yleft <- ifelse(grepl("tile", df$piece_side), df$y-1, df$y-0.5)
     yright <- ifelse(grepl("tile", df$piece_side), df$y+1, df$y+0.5)
     c(min(yleft), max(yright))
