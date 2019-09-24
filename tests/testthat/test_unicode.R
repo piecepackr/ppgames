ef9702aed3e5efb499b9f667e0dfb5080ea95eec
Trevor L Davis <trevor.l.davis@gmail.com>
2019-09-23 21:50:17 -0700

    refactor: Create lists to store character rotations
    Lists to store character rotations and box information tables.
    
    Closes #23

--- tests/testthat/test_unicode.R
+++ tests/testthat/test_unicode.R
@@ -7,6 +7,10 @@ test_that("dimensions", {
 })
 test_that("text diagrams", {
     skip_on_appveyor() 
+    expect_warning(rotate("$", 90))
+    expect_warning(rotate("&", 180))
+    expect_warning(rotate("&", 270))
+    expect_warning(rotate("&", 45))
     expect_output(cat_piece(df_fide_chess()),
                   "\u2600\u20dd\u2503\u263d\u20dd\u2502")
     expect_output(cat_piece(df_xiangqi()), 
