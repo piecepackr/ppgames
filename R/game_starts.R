bf7128e03df6aba7aca2fbc3c08d16a45110d430
Trevor Davis <trevor.l.davis@gmail.com>
2019-09-26 15:19:48 -0700

    feat: Allow passing in starting coins in 'df_fujisan'

--- R/game_starts.R
+++ R/game_starts.R
@@ -29,20 +29,43 @@ df_four_field_kono <- function(cfg1=pp_cfg()) {
     bind_rows(df_t, df_c)
 }
 
+random_fujisan_coins <- function(...) {
+    coins <- integer(24)
+    coins[which(1:24 %% 4 == 0)] <- sample(0:5)
+    coins[which(1:24 %% 4 == 1)] <- sample(0:5)
+    coins[which(1:24 %% 4 == 2)] <- sample(0:5)
+    coins[which(1:24 %% 4 == 3)] <- sample(0:5)
+    coins <- matrix(coins, nrow=2, byrow=TRUE)
+    coins
+}
+
 #' @rdname df_game
+#' @param coins String of coin layout
 #' @export
-df_fujisan <- function(seed=NULL) {
+df_fujisan <- function(seed=NULL, coins=NULL) {
+    old_seed <- .Random.seed
+    on.exit(.Random.seed <- old_seed)
     set.seed(seed)
+    if(is.null(coins)) {
+        coins <- random_fujisan_coins()
+    } else if (is.character(coins)) {
+        coins <- gsub("\n", "", coins)
+        coins <- gsub("\\\\", "", coins)
+        coins <- gsub(" ", "", coins)
+        coins <- gsub("n", "0", coins)
+        coins <- gsub("a", "1", coins)
+        coins <- as.integer(str_split(coins, "")[[1]])
+    }
+    if (is.vector(coins)) {
+        coins <- matrix(coins, nrow=2, byrow=TRUE)   
+    }
+    coins <- coins + 1
     df_t <- tibble(piece_side="tile_back", y=1.5,
                    x=1.5+c(seq(1,11,2),seq(2,10,2),seq(3,9,2),4,6,8,5,7,5,7,6,6))
     df_p <- tibble(piece_side="pawn_face", x=c(1,14,14,1), y=c(2,2,1,1), suit=1:4)
-    suit <- (0:23%%4)+1
-    df_c <- tibble(piece_side="coin_face", x=rep(13:2, 2), y=rep(1:2, each=12),
-                   suit=suit, rank=rep(1:6,4))
-    df_c[which(suit==1),"rank"] <- sample(1:6)
-    df_c[which(suit==2),"rank"] <- sample(1:6)
-    df_c[which(suit==3),"rank"] <- sample(1:6)
-    df_c[which(suit==4),"rank"] <- sample(1:6)
+    suit <- rev((0:23%%4)+1)
+    df_c <- tibble(piece_side="coin_face", x=rep(2:13, 2), y=rep(1:2, each=12),
+                   suit=suit, rank=c(coins[2,], coins[1,]))
     bind_rows(df_t, df_c, df_p)
 }
 
