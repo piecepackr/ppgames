a3b8a3205c6b86926740b6b6f0e1401c15cdfad4
Trevor Davis <trevor.l.davis@gmail.com>
2019-10-25 14:11:15 -0700

    chore: Use 'rlang::.data' to suppress check warning

--- R/range.R
+++ R/range.R
@@ -7,32 +7,39 @@ range_heuristic <- function(df) {
     list(xmin=min(xleft), xmax=max(xright), ymin=min(yleft), ymax=max(yright))
 }
 
+#' @importFrom rlang .data
 range_true <- function(df, cfg=pp_cfg(), envir=NULL, op_scale=0, op_angle=90, ...) {
     if(nrow(df) == 0) { return (c(NA_real_, NA_real_)) }
     df <- piecepackr:::add_3d_info(df, cfg, envir)
     df <- dplyr::mutate(df,
-                        xllb = piecepackr:::op_x(xll, yll, zb, op_angle, op_scale),
-                        xllt = piecepackr:::op_x(xll, yll, zt, op_angle, op_scale),
-                        xulb = piecepackr:::op_x(xul, yul, zb, op_angle, op_scale),
-                        xult = piecepackr:::op_x(xul, yul, zt, op_angle, op_scale),
-                        xlrb = piecepackr:::op_x(xlr, ylr, zb, op_angle, op_scale),
-                        xlrt = piecepackr:::op_x(xlr, ylr, zt, op_angle, op_scale),
-                        xurb = piecepackr:::op_x(xur, yur, zb, op_angle, op_scale),
-                        xurt = piecepackr:::op_x(xur, yur, zt, op_angle, op_scale),
-                        yllb = piecepackr:::op_y(xll, yll, zb, op_angle, op_scale),
-                        yllt = piecepackr:::op_y(xll, yll, zt, op_angle, op_scale),
-                        yulb = piecepackr:::op_y(xul, yul, zb, op_angle, op_scale),
-                        yult = piecepackr:::op_y(xul, yul, zt, op_angle, op_scale),
-                        ylrb = piecepackr:::op_y(xlr, ylr, zb, op_angle, op_scale),
-                        ylrt = piecepackr:::op_y(xlr, ylr, zt, op_angle, op_scale),
-                        yurb = piecepackr:::op_y(xur, yur, zb, op_angle, op_scale),
-                        yurt = piecepackr:::op_y(xur, yur, zt, op_angle, op_scale))
+                        xllb = piecepackr:::op_x(.data$xll, .data$yll, .data$zb, op_angle, op_scale),
+                        xllt = piecepackr:::op_x(.data$xll, .data$yll, .data$zt, op_angle, op_scale),
+                        xulb = piecepackr:::op_x(.data$xul, .data$yul, .data$zb, op_angle, op_scale),
+                        xult = piecepackr:::op_x(.data$xul, .data$yul, .data$zt, op_angle, op_scale),
+                        xlrb = piecepackr:::op_x(.data$xlr, .data$ylr, .data$zb, op_angle, op_scale),
+                        xlrt = piecepackr:::op_x(.data$xlr, .data$ylr, .data$zt, op_angle, op_scale),
+                        xurb = piecepackr:::op_x(.data$xur, .data$yur, .data$zb, op_angle, op_scale),
+                        xurt = piecepackr:::op_x(.data$xur, .data$yur, .data$zt, op_angle, op_scale),
+                        yllb = piecepackr:::op_y(.data$xll, .data$yll, .data$zb, op_angle, op_scale),
+                        yllt = piecepackr:::op_y(.data$xll, .data$yll, .data$zt, op_angle, op_scale),
+                        yulb = piecepackr:::op_y(.data$xul, .data$yul, .data$zb, op_angle, op_scale),
+                        yult = piecepackr:::op_y(.data$xul, .data$yul, .data$zt, op_angle, op_scale),
+                        ylrb = piecepackr:::op_y(.data$xlr, .data$ylr, .data$zb, op_angle, op_scale),
+                        ylrt = piecepackr:::op_y(.data$xlr, .data$ylr, .data$zt, op_angle, op_scale),
+                        yurb = piecepackr:::op_y(.data$xur, .data$yur, .data$zb, op_angle, op_scale),
+                        yurt = piecepackr:::op_y(.data$xur, .data$yur, .data$zt, op_angle, op_scale))
     dfr <- dplyr::summarize(df, 
-                            xmin=min(xl, xr), xmax=max(xl, xr),
-                            ymin=min(yb, yt), ymax=max(yb, yt),
-                            xmin_op=min(xllb, xllt, xulb, xult, xlrb, xlrt, xurb, xurt),
-                            xmax_op=max(xllb, xllt, xulb, xult, xlrb, xlrt, xurb, xurt),
-                            ymin_op=min(yllb, yllt, yulb, yult, ylrb, ylrt, yurb, yurt),
-                            ymax_op=max(yllb, yllt, yulb, yult, ylrb, ylrt, yurb, yurt))
+                            xmin=min(.data$xl, .data$xr), 
+                            xmax=max(.data$xl, .data$xr),
+                            ymin=min(.data$yb, .data$yt), 
+                            ymax=max(.data$yb, .data$yt),
+                            xmin_op=min(.data$xllb, .data$xllt, .data$xulb, .data$xult, 
+                                        .data$xlrb, .data$xlrt, .data$xurb, .data$xurt),
+                            xmax_op=max(.data$xllb, .data$xllt, .data$xulb, .data$xult, 
+                                        .data$xlrb, .data$xlrt, .data$xurb, .data$xurt),
+                            ymin_op=min(.data$yllb, .data$yllt, .data$yulb, .data$yult, 
+                                        .data$ylrb, .data$ylrt, .data$yurb, .data$yurt),
+                            ymax_op=max(.data$yllb, .data$yllt, .data$yulb, .data$yult, 
+                                        .data$ylrb, .data$ylrt, .data$yurb, .data$yurt))
     dfr
 }
