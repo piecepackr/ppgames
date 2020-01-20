2c4fd165f4303b2e35d44280dfe02b166beeacca
Trevor L Davis <trevor.l.davis@gmail.com>
2020-01-19 21:59:46 -0800

    fix: Update 'range_true' for piecepackr internal geometry changes

--- R/range.R
+++ R/range.R
@@ -8,29 +8,36 @@ range_heuristic <- function(df) {
 }
 
 #' @importFrom rlang .data
-range_true <- function(df, cfg = pp_cfg(), envir = NULL, op_scale = 0, op_angle = 90, ...) {
+range_true <- function(df, cfg = pp_cfg(), envir = NULL, op_scale = 0, op_angle = 45, ...) {
     if (nrow(df) == 0) {
         return(tibble::tibble(xmin = NA, xmax = NA, ymin = NA, ymax = NA,
                               xmin_op = NA, xmax_op = NA, ymin_op = NA, ymax_op = NA))
     }
     df <- piecepackr:::add_3d_info(df, cfg, envir)
-    df <- dplyr::mutate(df,
-                        xllb = piecepackr:::op_x(.data$xll, .data$yll, .data$zb, op_angle, op_scale),
-                        xllt = piecepackr:::op_x(.data$xll, .data$yll, .data$zt, op_angle, op_scale),
-                        xulb = piecepackr:::op_x(.data$xul, .data$yul, .data$zb, op_angle, op_scale),
-                        xult = piecepackr:::op_x(.data$xul, .data$yul, .data$zt, op_angle, op_scale),
-                        xlrb = piecepackr:::op_x(.data$xlr, .data$ylr, .data$zb, op_angle, op_scale),
-                        xlrt = piecepackr:::op_x(.data$xlr, .data$ylr, .data$zt, op_angle, op_scale),
-                        xurb = piecepackr:::op_x(.data$xur, .data$yur, .data$zb, op_angle, op_scale),
-                        xurt = piecepackr:::op_x(.data$xur, .data$yur, .data$zt, op_angle, op_scale),
-                        yllb = piecepackr:::op_y(.data$xll, .data$yll, .data$zb, op_angle, op_scale),
-                        yllt = piecepackr:::op_y(.data$xll, .data$yll, .data$zt, op_angle, op_scale),
-                        yulb = piecepackr:::op_y(.data$xul, .data$yul, .data$zb, op_angle, op_scale),
-                        yult = piecepackr:::op_y(.data$xul, .data$yul, .data$zt, op_angle, op_scale),
-                        ylrb = piecepackr:::op_y(.data$xlr, .data$ylr, .data$zb, op_angle, op_scale),
-                        ylrt = piecepackr:::op_y(.data$xlr, .data$ylr, .data$zt, op_angle, op_scale),
-                        yurb = piecepackr:::op_y(.data$xur, .data$yur, .data$zb, op_angle, op_scale),
-                        yurt = piecepackr:::op_y(.data$xur, .data$yur, .data$zt, op_angle, op_scale))
+    llb <- piecepackr:::Point3D$new(df$xll, df$yll, df$zb)$project_op(op_angle, op_scale)
+    llt <- piecepackr:::Point3D$new(df$xll, df$yll, df$zt)$project_op(op_angle, op_scale)
+    ulb <- piecepackr:::Point3D$new(df$xul, df$yul, df$zb)$project_op(op_angle, op_scale)
+    ult <- piecepackr:::Point3D$new(df$xul, df$yul, df$zt)$project_op(op_angle, op_scale)
+    lrb <- piecepackr:::Point3D$new(df$xlr, df$ylr, df$zb)$project_op(op_angle, op_scale)
+    lrt <- piecepackr:::Point3D$new(df$xlr, df$ylr, df$zt)$project_op(op_angle, op_scale)
+    urb <- piecepackr:::Point3D$new(df$xur, df$yur, df$zb)$project_op(op_angle, op_scale)
+    urt <- piecepackr:::Point3D$new(df$xur, df$yur, df$zt)$project_op(op_angle, op_scale)
+    df$xllb <- llb$x
+    df$yllb <- llb$y
+    df$xulb <- ulb$x
+    df$yulb <- ulb$y
+    df$xlrb <- lrb$x
+    df$ylrb <- lrb$y
+    df$xurb <- urb$x
+    df$yurb <- urb$y
+    df$xllt <- llt$x
+    df$yllt <- llt$y
+    df$xult <- ult$x
+    df$yult <- ult$y
+    df$xlrt <- lrt$x
+    df$ylrt <- lrt$y
+    df$xurt <- urt$x
+    df$yurt <- urt$y
     dfr <- dplyr::summarize(df,
                             xmin = min(.data$xl, .data$xr),
                             xmax = max(.data$xl, .data$xr),
