d3fd7c0eb8292c3fe1aa8d25d5a676ff36f23d4d
Trevor L Davis <trevor.l.davis@gmail.com>
2022-02-05 09:30:38 -0800

    fix: Skip 'verify_output()' on Windows
    * `verify_output()` tests throws CI errors on Windows
      so we skip them

--- tests/testthat/test_view_game.R
+++ tests/testthat/test_view_game.R
@@ -1,5 +1,9 @@
 context("test view game")
 test_that("view_game works as expected", {
+    g <- list(metadata = NULL, movetext = character())
+    g <- append_to_ppn(g, "1. S@b2")
+    expect_equal(nrow(g$dfs[["1."]]), 1)
+
     ppn <- read_ppn(system.file("ppn/tic-tac-toe.ppn", package = "ppgames"))
     game <- ppn[[1]]
 
@@ -16,13 +20,10 @@ test_that("view_game works as expected", {
     expect_equal(prev, "3...")
     expect_equal(next_move(game, prev), "4.")
 
+    skip_on_os("windows")
     print_screen <- function(...) ppgames:::print_screen(..., color = FALSE)
     verify_output("../text_diagrams/print_screen.txt",
                   print_screen(game, move, clear = FALSE))
-
-    g <- list(metadata = NULL, movetext = character())
-    g <- append_to_ppn(g, "1. S@b2")
-    expect_equal(nrow(g$dfs[["1."]]), 1)
 
     skip_if_not_installed("argparse")
     p <- get_parser()
