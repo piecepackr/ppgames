53cae7298f8d6af9d2989adca95d3e05920f48d6
Trevor L Davis <trevor.l.davis@gmail.com>
2022-02-07 21:10:42 -0800

    fix: Tweak plot_canonical_image()
    * Create `envir` that includes `cfg` and use that
      so image uses right configuration instead of
      `game_systems("sans")` which is not the prettiest option.

--- R/plot_canonical_image.R
+++ R/plot_canonical_image.R
@@ -1,9 +1,10 @@
 plot_canonical_image <- function(game, gk = game_kit(), file = NULL) {
     game <- gsub("-", "_", game)
     if (is.null(file)) file <- paste0(game, "_canonical.pdf")
+    cfg <- gk$get_piecepacks(1)[[1]]
+    envir <- list(piecepack = cfg)
     switch(game,
            pass_the_food = {
-               cfg <- gk$get_piecepacks(1)[[1]]
                df_tiles <- get_starting_df_from_name(game)
                df_tiles$id <- NULL
                df_tiles$cfg <- NULL
@@ -13,19 +14,20 @@ plot_canonical_image <- function(game, gk = game_kit(), file = NULL) {
                                   y = stats::runif(24, -1.20, 13.50),
                                   angle = stats::runif(24, 0, 360))
                df <- bind_rows(df_tiles, df_coins)
-               wh <- render_piece(df, file = file, annotate = FALSE, cfg = cfg,
+               wh <- render_piece(df, file = file, annotate = FALSE,
+                                  envir = envir,
                                   op_scale = 0.5, trans = op_transform)
            },
            tablut = {
-               cfg <- gk$get_piecepacks(1)[[1]]
                df <- df_tablut(cfg$get_width("die_face"))
-               wh <- render_piece(df, file = file, annotate = FALSE, cfg = cfg,
-                             op_scale = 0.5, trans = op_transform)
+               wh <- render_piece(df, file = file, annotate = FALSE,
+                                  envir = envir,
+                                  op_scale = 0.5, trans = op_transform)
 
            }, {
-               cfg <- gk$get_piecepacks(1)[[1]]
                df <- get_starting_df_from_name(game)
-               wh <- render_piece(df, file = file, annotate = FALSE, cfg = cfg,
+               wh <- render_piece(df, file = file, annotate = FALSE,
+                                  envir = envir,
                                   op_scale = 0.5, trans = op_transform)
            })
     invisible(c(wh, list(file = file)))
